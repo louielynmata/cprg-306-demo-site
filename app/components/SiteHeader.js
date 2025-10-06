21a78e7aa645190cd04a9579365e2912207b905e
Ashlyn Knox <ashx3s@tuta.io>
2025-10-06 13:22:14 -0600

    week 5 added to nav

--- app/components/SiteHeader.js
+++ app/components/SiteHeader.js
@@ -18,6 +18,9 @@ export default function SiteHeader() {
           <li>
             <Link href="week-4">Week 4</Link>
           </li>
+          <li>
+            <Link href="week-5">Week 5</Link>
+          </li>
         </ul>
       </nav>
     </header>
