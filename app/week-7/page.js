29de9b06e0c73e750342bf0684158349bd77ef31
Ashlyn Knox <ashx3s@tuta.io>
2025-10-15 11:48:36 -0600

    immutable state update example of a single object

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,48 +1,49 @@
-const characters = ["Bee", "Planet", "Chipmunk", "Brain"];
+"use client";
+import { useState } from "react";
 
-const coffeeVarieties = [
-  { id: 232, name: "Brazilian Blitz Bonanza", cost: "3.45", quantity: 0 },
-  { id: 233, name: "Sumatra Party", cost: "5.30", quantity: 0 },
-  { id: 234, name: "Tiny Tim's Toolshed", cost: "2.20", quantity: 0 },
-];
-
-function SimpleArrayDemoSection() {
-  return (
-    <section>
-      <h2>Simple Array</h2>
-      <p>
-        Using index isn't great but better than character due to possible
-        duplication
-      </p>
-      <ul>
-        {characters.map((character, index) => (
-          <li key={index}>{character}</li>
-        ))}
-      </ul>
-      <ol>
-        {characters.map((character, index) => {
-          return (
-            <li key={index + 1} className="m-2 text-lg">
-              {character}
-            </li>
-          );
-        })}
-      </ol>
-    </section>
-  );
-}
 export default function Page() {
+  const coffeeVarieties = [
+    { id: 232, name: "Brazilian Blitz Bonanza", cost: "3.45", quantity: 0 },
+    { id: 233, name: "Sumatra Party", cost: "5.30", quantity: 0 },
+    { id: 234, name: "Tiny Tim's Toolshed", cost: "2.20", quantity: 0 },
+  ];
+  const [coffees, setCoffees] = useState([...coffeeVarieties]);
+
+  const [coffeeOfTheDay, setCoffeeOfTheDay] = useState(coffeeVarieties[0]);
+
+  function updateCoffeeOfTheDayQuantity() {
+    setCoffeeOfTheDay({
+      ...coffeeOfTheDay,
+      quantity: coffeeOfTheDay.quantity + 1,
+    });
+  }
+
   return (
     <main>
       <header>
         <h1>Exploring State with Objects</h1>
         <p>review of list rendering as well</p>
       </header>
-      <SimpleArrayDemoSection />
+      <section>
+        <header>
+          <h2>Exploring Immutability</h2>
+        </header>
+        <div className="my-12">
+          <h3>Coffee of the Day: {coffeeOfTheDay.name} </h3>
+          <p>{coffeeOfTheDay.cost}</p>
+          <p>{coffeeOfTheDay.quantity}</p>
+          <button
+            className="px-4 py-2 mt-4 rounded-lg bg-pink-800 cursor-pointer"
+            onClick={updateCoffeeOfTheDayQuantity}
+          >
+            Increase Quantity
+          </button>
+        </div>
+      </section>
       <section className="my-4">
         <h2 className="mb-2 text-2xl font-semibold">Array of Objects</h2>
         <ul>
-          {coffeeVarieties.map((coffee) => {
+          {coffees.map((coffee) => {
             return (
               <li
                 key={coffee.id}
@@ -53,6 +54,7 @@ export default function Page() {
                   <p>{coffee.cost}</p>
                   <p className="font-semibold text-lg">{coffee.quantity}</p>
                 </div>
+                {/* TODO: Add stateful button for individual array items */}
                 <button className="px-4 py-2 mt-4 rounded-lg bg-pink-800 cursor-pointer">
                   Increase Quantity
                 </button>
