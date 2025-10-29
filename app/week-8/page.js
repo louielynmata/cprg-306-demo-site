0291d84be2afe2e7503f09f4753b0f5cc9728712
Ashlyn Knox <ashx3s@tuta.io>
2025-10-29 11:25:14 -0600

    useCounter code and refactor
    example code and optimization of page organization

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -22,7 +22,7 @@ export default function Page() {
   }
   useEffect(() => {
     fetchCharacters();
-  }, []);
+  }, [characters]);
 
   if (error) {
     return (
---
+++ app/week-9/page.js
@@ -0,0 +1,14 @@
+import CounterDemoSection from "./CounterDemoSection";
+
+export default function Page() {
+  return (
+    <main>
+      {/* TODO: Use PageHeader component */}
+      <header>
+        <h1>Week 9 Part 1</h1>
+        <p>Custom hooks and use context</p>
+      </header>
+      <CounterDemoSection />
+    </main>
+  );
+}
