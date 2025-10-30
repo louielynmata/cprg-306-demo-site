6b07deb354e185098ebae10e867c1dbf29e8997f
Ashlyn Knox <ashx3s@tuta.io>
2025-10-30 15:26:43 -0600

    reset week 9 demos

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,19 +1,3 @@
-import CounterDemoSection from "./CounterDemoSection";
-import DataFetchDemoSection from "./DataFetchDemoSection";
-const PageHeader = () => {
-  return (
-    <header>
-      <h1>Week 9 Part 1</h1>
-      <p>Custom hooks and use context</p>
-    </header>
-  );
-};
 export default function Page() {
-  return (
-    <main>
-      <PageHeader />
-      <CounterDemoSection />
-      <DataFetchDemoSection />
-    </main>
-  );
+  return <main></main>;
 }
