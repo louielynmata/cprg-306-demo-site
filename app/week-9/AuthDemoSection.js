9c01ba4f48efe2b63267a818526b2c8bce72b1fb
Ashlyn Knox <ashx3s@tuta.io>
2025-11-04 15:53:14 -0700

    setup firebase config

---
+++ app/week-9/AuthDemoSection.js
@@ -0,0 +1,12 @@
+export default function AuthDemoSection() {
+  return (
+    <section className="my-8">
+      <header>
+        <h2>Authenticated User on Firebase Example</h2>
+        {/* Todo: show user email here */}
+        <p>User Email: </p>
+      </header>
+      {/* TODO: Add Login and Signup forms here */}
+    </section>
+  );
+}
